b3f485ddccb2ad1167e616b3ab0c860ce98870f6
Thibault Baron <thibault.baron56@gmail.com>
2017-02-04 19:09:12 +0100

    Controle des fils separe, ajustements physiques, tete stable

--- Assets/PuppetString.cs
+++ Assets/PuppetString.cs
@@ -7,37 +7,47 @@ public class PuppetString : MonoBehaviour {
 	public GameObject attachedObject;
 	private Rigidbody rb;
 	public float speed = 4;
-	public ForceMode mode;
 	public float length {
 		get;
 		private set;
 	}
 	[SerializeField]
 	[Range(10000f, 100000f)]
-	float factor = 10000f;
+	float springConstant = 10000f;
 	private float relaxedLength;
+	private Vector3 lastVelocity;
+
+	public string controllerExtension = "L";
 
 	void Start () {
+		if (!attachedObject)
+			return;
 		rb = attachedObject.GetComponent<Rigidbody> ();
 		lr = GetComponent<LineRenderer> ();
 		relaxedLength = Vector3.SqrMagnitude (transform.position - attachedObject.transform.position);
 	}
 
 	void Update () {
+		if (!attachedObject)
+			return;
 		lr.SetPosition (0, transform.position);
 		lr.SetPosition (1, attachedObject.transform.position);
-		if (Vector3.SqrMagnitude (transform.position - attachedObject.transform.position) > relaxedLength) {
-			lr.SetColors (Color.red, Color.red);
-		} else {
-			lr.SetColors (Color.green, Color.green);
-		}
 	}
 	
-	void FixedUpdate () {	
-		transform.position += new Vector3 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, Input.GetAxis ("Vertical") * speed * Time.deltaTime, 0f);
+	void FixedUpdate () {
+		if (controllerExtension.Length > 0)
+			transform.position += Vector3.Normalize (new Vector3 (Input.GetAxis ("Horizontal" + controllerExtension), 0f, Input.GetAxis ("Vertical" + controllerExtension))) * speed * Time.deltaTime;
 		if (rb) {
 			//rb.velocity = Vector3.zero;
-			rb.AddForce ((transform.position - attachedObject.transform.position) * Time.fixedDeltaTime * factor, mode);
+			/*
+			acceleration = (rb.velocity - lastVelocity) / Time.fixedDeltaTime;
+			lastVelocity = rb.velocity;//*/
+			if (Vector3.SqrMagnitude (transform.position - attachedObject.transform.position) > relaxedLength) {
+				rb.AddForce ((transform.position - attachedObject.transform.position) * Time.fixedDeltaTime * springConstant, ForceMode.Force);
+				lr.SetColors (Color.red, Color.red);
+			} else {
+				lr.SetColors (Color.green, Color.green);
+			}
 		}
 	}
 }
