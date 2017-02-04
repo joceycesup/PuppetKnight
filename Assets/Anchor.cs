b51fdef2d337ccd52342a2541c8b69c20b8084bd
Thibault Baron <thibault.baron56@gmail.com>
2017-02-04 01:49:19 +0100

    Initialisation projet Premiere version des elastiques presente, ragdoll attache au fils.

---
+++ Assets/Anchor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Rigidbody))]
+[RequireComponent(typeof (LineRenderer))]
+public class Anchor : MonoBehaviour {
+	private Rigidbody rb;
+	private LineRenderer lr;
+	public ForceMode mode;
+	public Transform stringAnchor;
+	[SerializeField]
+	[Range(10000f, 100000f)]
+	float factor = 10000f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		lr = GetComponent<LineRenderer> ();
+	}
+
+	void Update () {
+		lr.SetPosition (0, transform.position);
+		lr.SetPosition (1, stringAnchor.position);
+	}
+
+	void FixedUpdate () {
+		if (stringAnchor) {
+			//rb.velocity = Vector3.zero;
+			rb.AddForce ((stringAnchor.position - transform.position) * Time.fixedDeltaTime * factor, mode);
+		}
+		//rb.AddForce(Vector3.up*Input.GetAxis("Vertical")*500);
+	}
+}
---
+++ Assets/PuppetString.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (LineRenderer))]
+public class PuppetString : MonoBehaviour {
+	private LineRenderer lr;
+	public GameObject attachedObject;
+	private Rigidbody rb;
+	public float speed = 4;
+	public ForceMode mode;
+	public float length {
+		get;
+		private set;
+	}
+	[SerializeField]
+	[Range(10000f, 100000f)]
+	float factor = 10000f;
+	private float relaxedLength;
+
+	void Start () {
+		rb = attachedObject.GetComponent<Rigidbody> ();
+		lr = GetComponent<LineRenderer> ();
+		relaxedLength = Vector3.SqrMagnitude (transform.position - attachedObject.transform.position);
+	}
+
+	void Update () {
+		lr.SetPosition (0, transform.position);
+		lr.SetPosition (1, attachedObject.transform.position);
+		if (Vector3.SqrMagnitude (transform.position - attachedObject.transform.position) > relaxedLength) {
+			lr.SetColors (Color.red, Color.red);
+		} else {
+			lr.SetColors (Color.green, Color.green);
+		}
+	}
+	
+	void FixedUpdate () {	
+		transform.position += new Vector3 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, Input.GetAxis ("Vertical") * speed * Time.deltaTime, 0f);
+		if (rb) {
+			//rb.velocity = Vector3.zero;
+			rb.AddForce ((transform.position - attachedObject.transform.position) * Time.fixedDeltaTime * factor, mode);
+		}
+	}
+}
